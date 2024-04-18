3f281d1d694b053c6e962fd541c28c26f25850b4
CarlosOtrebla <carlosotrebla@gmail.com>
2024-04-18 13:34:25 -0300

    Fazendo o cadastro só aceitar os dados corretos

--- SistemaBancario/src/io/Leitores.java
+++ SistemaBancario/src/io/Leitores.java
@@ -18,6 +18,9 @@ import pessoas.Gerente;
 import pessoas.Presidente;
 
 public class Leitores {
+	public static final String RESET = "\u001B[0m";
+	public static final String CYAN = "\u001B[36m";
+	public static final String YELLOW = "\u001B[33m";
 
 	public static void escritor(String path, double valor) throws IOException {
 		BufferedWriter buffWrite = new BufferedWriter(new FileWriter(path, true));
@@ -29,34 +32,94 @@ public class Leitores {
 	public static void CriarConta() throws IOException, InterruptedException {
 		Scanner sc = new Scanner(System.in);
 		BufferedWriter buffWrite = new BufferedWriter(new FileWriter("Cadastros.txt", true));
-
+		boolean continuar = true;
 		String linha = "";
+		do {
+			System.out.println(CYAN+" *** Crie sua senha: "+RESET);
+			linha = sc.nextLine().trim();
+			if (linha.isEmpty()) {
+				System.out.println(YELLOW + "!!! VOCÊ PRECISA DEFINIR UMA SENHA !!!\n" + RESET);
+				Thread.sleep(1500);
 
-		System.out.println(" *** Crie sua senha: ");
-		linha = sc.nextLine();
-		buffWrite.append(linha + ";");
-		System.out.println("");
-
-		System.out.println(" *** Digite seu CPF: ");
-		linha = sc.nextLine();
-		buffWrite.append(linha + ";");
-		System.out.println("");
-
-		System.out.println(" *** Digite seu Numero da Conta: ");
-		linha = sc.nextLine();
-		buffWrite.append(linha + ";");
-		System.out.println("");
+			} else {
+				buffWrite.append(linha + ";");
+				System.out.println("");
+				continuar = false;
+			}
+		} while (continuar);
 		
-		System.out.println(" *** Digite seu Nome: ");
-		linha = sc.nextLine();
-		buffWrite.append(linha + ";");
-		System.out.println("");
+		continuar = true;
 		
-		System.out.println(" *** Caso cliente, digite 1 ║ Caso funcionario, digite 0 : ");
-		String Tipo = sc.nextLine();
-		switch (Tipo) {
+		do {
+			System.out.println(CYAN+ " *** Digite seu CPF com 11 números: "+RESET);
+			linha = sc.nextLine().trim();
+			if (linha.isEmpty()) {
+				System.out.println(YELLOW + "!!! VOCÊ PRECISA INFORMAR SEU CPF !!!\n" + RESET);
+				Thread.sleep(1500);
+			} else if(!linha.matches("[0-9]{11}")) {
+				System.out.println(YELLOW + "!!! ENTRADA INVÁLIDA PARA CPF !!!\n" + RESET);
+				Thread.sleep(1500);
+			} else {
+				buffWrite.append(linha + ";");
+				System.out.println("");
+				continuar = false;
+			}
+		} while (continuar);
+		
+		continuar = true;
+		
+		do {
+			System.out.println(CYAN + " *** Digite seu Numero da Conta: "+RESET);
+			linha = sc.nextLine().trim();
+			if (linha.isEmpty()) {
+				System.out.println(YELLOW + "!!! VOCÊ PRECISA DEFINIR UM NÚMERO PRA CONTA !!!\n" + RESET);
+				Thread.sleep(1500);
+
+			} else {
+				buffWrite.append(linha + ";");
+				System.out.println("");
+				continuar = false;
+			}
+		} while (continuar);
+		
+		continuar = true;
+
+		do {
+			System.out.println(CYAN + " *** Digite seu Nome: "+RESET);
+			linha = sc.nextLine().trim();
+			if (linha.isEmpty()) {
+				System.out.println(YELLOW + "!!! VOCÊ PRECISA INFORMAR SEU NOME !!!\n" + RESET);
+				Thread.sleep(1500);
+			} else if (!linha.matches("[a-zA-ZÀ-ÿ\\s]+")) {
+		        System.out.println(YELLOW + "!!! ENTRADA INVÁLIDA PARA NOME (SOMENTE LETRAS E ESPAÇOS) !!!\n" + RESET);
+		        Thread.sleep(1500);
+		    }
+			else {
+				buffWrite.append(linha + ";");
+				System.out.println("");
+				continuar = false;
+			}
+		} while (continuar);
+		
+		continuar = true;
+		String tipo;
+		
+		do {
+			System.out.println(CYAN +" *** Caso cliente, digite 1 ║ Caso funcionario, digite 0 : "+ RESET);
+			tipo = sc.nextLine().trim();
+
+			if (tipo.isEmpty() || !tipo.matches("[0-1]")) {
+				System.out.println(YELLOW + "!!! Opção inválida. Por favor, escolha uma opção válida !!!\n" + RESET);
+				Thread.sleep(2000);
+
+			} else {
+				continuar = false;
+			}
+		} while (continuar);
+
+		switch (tipo) {
 		case "1":
-			System.out.println("Caso seja uma Conta Poupança, digite 1 | Caso seja uma conta corrente digite 0");
+			System.out.println(CYAN+"*** Caso seja uma Conta Poupança, digite 1 | Caso seja uma conta corrente digite 0 (ZERO)***"+RESET);
 			String Tipo_Conta = sc.nextLine();
 			System.out.println("");
 			switch (Tipo_Conta) {
@@ -67,7 +130,7 @@ public class Leitores {
 				buffWrite.append(depositoInicial + ";");
 				System.out.println("Me diga sua Agência");
 				String agencia1 = sc.nextLine();
-				
+
 				buffWrite.append(agencia1 + ";");
 				break;
 			case "0":
@@ -78,7 +141,7 @@ public class Leitores {
 				System.out.println("Me diga sua Agência");
 				String agencia2 = sc.nextLine();
 				buffWrite.append(agencia2 + ";");
-				
+
 				break;
 			}
 			break;
@@ -118,10 +181,10 @@ public class Leitores {
 		buffWrite.append("\n");
 		buffWrite.close();
 		System.out.println("\n\n");
-		System.out.println("*** Conta criada com sucesso! ***");
+		System.out.println(YELLOW +"*** Conta criada com sucesso! ***"+ RESET);
 		System.out.println("\n");
 		Thread.sleep(3000);
-		
+
 	}
 
 	public static Conta login(String senha, String Cpf) {
@@ -134,19 +197,22 @@ public class Leitores {
 				if (lista[0].trim().equalsIgnoreCase(senha) && lista[1].trim().equalsIgnoreCase(Cpf)) {
 					if (lista[4].equalsIgnoreCase("Poupanca")) {
 						ler.close();
-						return new ContaPoupanca(lista[0], lista[1], lista[2], lista[3], Double.parseDouble(lista[5]), lista[6]);
+						return new ContaPoupanca(lista[0], lista[1], lista[2], lista[3], Double.parseDouble(lista[5]),
+								lista[6]);
 
 					} else if (lista[4].equalsIgnoreCase("Corrente")) {
 						ler.close();
-						return new ContaCorrente(lista[0], lista[1], lista[2], lista[3], Double.parseDouble(lista[5]), lista[6]);
+						return new ContaCorrente(lista[0], lista[1], lista[2], lista[3], Double.parseDouble(lista[5]),
+								lista[6]);
 
 					} else if (lista[4].equalsIgnoreCase("Gerente")) {
 						ler.close();
-						return new Gerente(lista[0], lista[1], lista[2], lista[3], Double.parseDouble(lista[5]), lista[6]);
-					} else if(lista[4].equalsIgnoreCase("Diretor")) {
+						return new Gerente(lista[0], lista[1], lista[2], lista[3], Double.parseDouble(lista[5]),
+								lista[6]);
+					} else if (lista[4].equalsIgnoreCase("Diretor")) {
 						ler.close();
 						return new Diretor(lista[0], lista[1], lista[2], lista[3], Double.parseDouble(lista[5]));
-					} else if(lista[4].equalsIgnoreCase("Presidente")) {
+					} else if (lista[4].equalsIgnoreCase("Presidente")) {
 						ler.close();
 						return new Presidente(lista[0], lista[1], lista[2], lista[3], Double.parseDouble(lista[5]));
 					} else {
@@ -162,109 +228,110 @@ public class Leitores {
 		}
 		return null;
 	}
-	
-	//Retorna Conta Cliente
+
+	// Retorna Conta Cliente
 	public static Conta RetornaConta(String NumeroConta) {
 		try {
 			BufferedReader ler = new BufferedReader(new FileReader("Cadastros.txt"));
 			String linha;
-			while((linha = ler.readLine()) != null) {
+			while ((linha = ler.readLine()) != null) {
 				String[] lista = linha.split(";");
-				if(lista[2].equalsIgnoreCase(NumeroConta)) {
+				if (lista[2].equalsIgnoreCase(NumeroConta)) {
 					if (lista[4].equalsIgnoreCase("Poupanca")) {
 						ler.close();
-						return new ContaPoupanca(lista[0], lista[1], lista[2], lista[3], Double.parseDouble(lista[5]), lista[6]);
+						return new ContaPoupanca(lista[0], lista[1], lista[2], lista[3], Double.parseDouble(lista[5]),
+								lista[6]);
 
-						} else if (lista[4].equalsIgnoreCase("Corrente")) {
-							ler.close();
-							return new ContaCorrente(lista[0], lista[1], lista[2], lista[3], Double.parseDouble(lista[5]), lista[6]);
-						} else if (lista[4].equalsIgnoreCase("Gerente")) {
-							ler.close();
-							return new Gerente(lista[0], lista[1], lista[2], lista[3], Double.parseDouble(lista[5]), lista[6]);
-						} else if(lista[4].equalsIgnoreCase("Diretor")) {
-							ler.close();
-							return new Diretor(lista[0], lista[1], lista[2], lista[3], Double.parseDouble(lista[5]));
-						} else if(lista[4].equalsIgnoreCase("Presidente")) {
-							ler.close();
-							return new Presidente(lista[0], lista[1], lista[2], lista[3], Double.parseDouble(lista[5]));
-						} else {
-							continue;
-						}
+					} else if (lista[4].equalsIgnoreCase("Corrente")) {
+						ler.close();
+						return new ContaCorrente(lista[0], lista[1], lista[2], lista[3], Double.parseDouble(lista[5]),
+								lista[6]);
+					} else if (lista[4].equalsIgnoreCase("Gerente")) {
+						ler.close();
+						return new Gerente(lista[0], lista[1], lista[2], lista[3], Double.parseDouble(lista[5]),
+								lista[6]);
+					} else if (lista[4].equalsIgnoreCase("Diretor")) {
+						ler.close();
+						return new Diretor(lista[0], lista[1], lista[2], lista[3], Double.parseDouble(lista[5]));
+					} else if (lista[4].equalsIgnoreCase("Presidente")) {
+						ler.close();
+						return new Presidente(lista[0], lista[1], lista[2], lista[3], Double.parseDouble(lista[5]));
+					} else {
+						continue;
+					}
 				}
-			}	
-			
-			
+			}
+
 		} catch (IOException e) {
 			e.printStackTrace();
 			return null;
 		}
 		return null;
 	}
-	
+
 	@SuppressWarnings("null")
 	public static double RelatorioPresidente() {
 		double Valor = 0;
-		
+
 		try {
-		BufferedReader ler = new BufferedReader(new FileReader("Cadastros.txt"));
-		String linha = "";
-		while ((linha = ler.readLine()) != null) {
-			String[] lista = linha.split(";");
-			Valor += Double.parseDouble(lista[5]);
-		}
-		return Valor;
-		}catch(IOException e){
+			BufferedReader ler = new BufferedReader(new FileReader("Cadastros.txt"));
+			String linha = "";
+			while ((linha = ler.readLine()) != null) {
+				String[] lista = linha.split(";");
+				Valor += Double.parseDouble(lista[5]);
+			}
+			return Valor;
+		} catch (IOException e) {
 			e.printStackTrace();
 			return (Double) null;
 		}
 	}
-	
+
 	public static void RelatorioGerente(String Agencia) {
 		int Quantidade = 0;
-		
+
 		try {
-		BufferedReader ler = new BufferedReader(new FileReader("Cadastros.txt"));
-		String linha = "";
-		while ((linha = ler.readLine()) != null) {
-			String[] lista = linha.split(";");
-			if(!lista[4].equals("Diretor") && !lista[4].equals("Presidente")) {
-				if(lista[6].equals(Agencia)) {
-					Quantidade++;
+			BufferedReader ler = new BufferedReader(new FileReader("Cadastros.txt"));
+			String linha = "";
+			while ((linha = ler.readLine()) != null) {
+				String[] lista = linha.split(";");
+				if (!lista[4].equals("Diretor") && !lista[4].equals("Presidente")) {
+					if (lista[6].equals(Agencia)) {
+						Quantidade++;
+					}
+				} else {
+					continue;
 				}
 			}
-			else {
-				continue;
-			}
-		}
-		
-		System.out.println("Esse gerente possuí " + Quantidade + " Clientes em sua Agência");
-		}catch(IOException e){
-			
+
+			System.out.println("Esse gerente possuí " + Quantidade + " Clientes em sua Agência");
+		} catch (IOException e) {
+
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void RelatorioDiretor() {
 		try {
-		BufferedReader ler = new BufferedReader(new FileReader("Cadastros.txt"));
-		HashMap<String, String> Nomes = new HashMap<>();
-		String linha;
-		while ((linha = ler.readLine()) != null) {
+			BufferedReader ler = new BufferedReader(new FileReader("Cadastros.txt"));
+			HashMap<String, String> Nomes = new HashMap<>();
+			String linha;
+			while ((linha = ler.readLine()) != null) {
 				String[] lista = linha.split(";");
-				if(lista[4].equals("Poupanca") || lista[4].equals("Corrente")) {
+				if (lista[4].equals("Poupanca") || lista[4].equals("Corrente")) {
 					String Lista_aux = ("Nome: " + lista[3] + " Cpf: " + lista[1] + " Agência: " + lista[6]);
 					Nomes.put(lista[3], Lista_aux);
 				}
 			}
-		ler.close();
-		 ArrayList<String> nomesOrdenados = new ArrayList<>(Nomes.keySet());
-         Collections.sort(nomesOrdenados);
-		
-         for(String nome : nomesOrdenados) {
-        	 System.out.println(Nomes.get(nome));
-         }
-		
-		}catch (IOException e) {
+			ler.close();
+			ArrayList<String> nomesOrdenados = new ArrayList<>(Nomes.keySet());
+			Collections.sort(nomesOrdenados);
+
+			for (String nome : nomesOrdenados) {
+				System.out.println(Nomes.get(nome));
+			}
+
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
